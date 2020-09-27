0f450a9bd9398126c6c65891380e0c58868cc7d1
odnoo <odnoo861@gmail.com>
2020-09-27 23:41:28 +0800

    dice toll towch darahad shoonii zurag soligdono

--- app.js
+++ app.js
@@ -13,35 +13,27 @@ var p2TotalScore = 0;
 //Тоглогчийн ээжлийн оноог хадгалах
 var roundScore = 0;
 
-//Шооны аль талаараа буусныг хадгалах
-var dice = Math.floor(Math.random()*6+1);
+//dice object in DOM ()
+var diceDom = document.querySelector(".dice");
 
 
 //preparing to start
-document.querySelector(".dice").style.display = "none";
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-
-
-// document.querySelector(".dice").src = "dice-"+dice+".png";
-
-console.log(dice);
-
-//dice png array
-var dicePng = [
-    "dice-1.png",
-    "dice-2.png",
-    "dice-3.png",
-    "dice-4.png",
-    "dice-5.png",
-    "dice-6.png"
-]
+diceDom.style.display = "none";
 
 
-function getScrore(){
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    
+ //Шооны аль талаараа буусныг хадгалах
+var diceNumber = Math.floor(Math.random()*6+1);
 
-}
+diceDom.style.display = "block";
+diceDom.src = "dice-"+diceNumber+".png";
+console.log(diceNumber);
+
+});
